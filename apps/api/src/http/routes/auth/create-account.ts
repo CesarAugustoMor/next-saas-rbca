128236b9939e99006a94d7ca24e0c761efff0b00
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2026-01-12 16:34:07 -0300

    rote to create user

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,5 +1,7 @@
+import { hash } from "bcryptjs";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { prisma } from "@/lib/prisma";
 import z from "zod";
 
 export async function createAccount(app:FastifyInstance) {
@@ -12,6 +14,25 @@ export async function createAccount(app:FastifyInstance) {
             })
         }
     }, async (request, reply) => {
-        return { message: "User created successfully" };
+        const { name, email, password } = request.body;
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email }
+        });
+        if (existingUser) {
+            return reply.status(400).send({ error: "User with same email already exists" });
+        }
+
+        const hashedPassword =  await hash(password, 6); // numero varia de acordo com o nivel de seguranca (performance)
+
+        await prisma.user.create({
+            data: {
+                name,
+                email,
+                hashedPassword,
+            }
+        });
+
+        return reply.status(201).send({ message: "User created successfully" });
     });
 };
