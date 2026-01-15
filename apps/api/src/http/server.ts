60cb032b0aa94ea683aa30aa151a048553d2b6a1
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2026-01-15 19:02:05 -0300

    Route: Accept invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,9 +18,10 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
-import { getInvites } from './routes/invites/get-invaites'
 import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -108,6 +109,7 @@ app.register(removeMember)
 app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
+app.register(acceptInvite)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server is running on http://localhost:${env.PORT}`)
