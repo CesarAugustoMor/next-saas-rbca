65ac6b2d91899e6d6f655a64795f701094a57032
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2026-01-20 20:09:03 -0300

    Themes dark/light with Next-themes

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,7 @@
-import type { Metadata } from "next";
 import "./globals.css";
+
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,11 +14,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body
-      >
-        {children}
+    <html suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+        >
+          {children}
+          </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
