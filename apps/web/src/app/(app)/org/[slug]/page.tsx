d48feedaf8531393136d289c98c682133476927c
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2026-01-27 19:59:43 -0300

    App navigation using tabs and permissions

--- apps/web/src/app/(app)/org/[slug]/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -2,7 +2,8 @@
 export default async function Projects() {
 
   return (
-    <div >
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Project</h1>
     </div>
-  );
+  )
 }
