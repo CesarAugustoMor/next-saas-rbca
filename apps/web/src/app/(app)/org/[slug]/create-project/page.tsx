3c52af2478f31042e8fa5178446794ac35695ea1
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2026-01-26 21:14:37 -0300

    Permissions for create a project

--- apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/create-project/page.tsx
@@ -1,14 +1,21 @@
-import { Header } from "@/components/header";
-import { ProjectForm } from './project-form';
+import { redirect } from 'next/navigation'
 
-export default function CreateProject(){
+import { ability } from '@/auth/auth'
+
+import { ProjectForm } from './project-form'
+
+export default async function CreateProject() {
+  const permissions = await ability()
+
+  if (permissions?.cannot('create', 'Project')) {
+    redirect('/')
+  }
+
   return (
-    <div className="space-y-4 py-4">
-      <main className="mx-auto w-full max-w-[1200px] space-y-4">
-        <h1 className="text-2xl font-bold">Create project</h1>
+    <div className="space-y-4">
+      <h1 className="text-2xl font-bold">Create project</h1>
 
-        <ProjectForm />
-      </main>
+      <ProjectForm />
     </div>
   )
 }
